f37bdb17c10f583795e1eb8c52ecdeff84b97292
Gagana004 <gaganakavindu96@gmail.com>
2021-08-23 09:56:52 +0530

    finalizing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import  { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
-import Sidebar from "./components/sidebar"
+import Sidebar from "./components/sidebar";
+import FilesView from "./components/filesView/FilesView";
+import SideIcons from "./components/sideIcons"
+
+import GDriveLogo from './media/Google_Drive_icon.png'
+import { auth, provider } from "./firebase";
 
 function App() {
-  
-  const [user, setUser] = useState({
-    displayName: "David Rakosi",
-    email: "david@cleverprogrammer.com",
-    emailVerified: true,
-    phoneNumber: null,
-    photoURL: "https://lh6.googleusercontent.com/-KyLTWqvDIHQ/AAAAAAAAAAI/AAAAAAAAAAA/AMZuuclcWGWqkt6YUAan32YO4CSR07Y2jw/s96-c/photo.jpg",
-  });
-  return (
-    <div className="App">
-      <Header userPhoto={user.photoURL} />
-      <Sidebar />
-    </div>
-  );
+	const [user, setUser] = useState()
+
+	const handleLogin = () => {
+		if (!user) {
+			auth.signInWithPopup(provider).then((result) => {
+				setUser(result.user)
+				console.log(result.user)
+			}).catch((error) => {
+				alert(error.message);
+			});
+		} else if (user) {
+			auth.signOut().then(() => {
+				setUser(null)
+			}).catch((err) => alert(err.message))
+		}
+	}
+
+	return (
+		<div className="app">
+			{
+				user ? (
+					<>
+						<Header userPhoto={user.photoURL} />
+						<div className="app__main">
+							<Sidebar />
+							<FilesView />
+							<SideIcons />
+						</div>
+					</>
+				) : (
+					<div className='app__login'>
+						<div className="app__login--logo">
+							<img src={GDriveLogo} alt="Google Drive" />
+						</div>
+						<div className="loginbutton">
+							<button className="loginButton" onClick={handleLogin}>Log in to Google Drive</button>
+						</div>
+					</div>
+				)
+			}
+		</div>
+	);
 }
 
 export default App;
